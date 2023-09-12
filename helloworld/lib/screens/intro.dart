9601a12714e5724e86dcd95980fc7c33196248de
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-09-12 08:22:49 +0700

    Update intro.dart

--- helloworld/lib/screens/intro.dart
+++ helloworld/lib/screens/intro.dart
@@ -31,13 +31,13 @@ class _IntroScreenState extends State<IntroScreen>
       height: 812,
       padding: const EdgeInsets.symmetric(horizontal: 102),
       clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(color: Color(0xFF121212)),
+      decoration: const BoxDecoration(color: Color(0xFF121212)),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
+          SizedBox(
             width: 171,
             height: 171,
             child: Column(
@@ -49,79 +49,79 @@ class _IntroScreenState extends State<IntroScreen>
                   width: 171,
                   height: 171,
                   decoration: ShapeDecoration(
-                    color: Color(0xFFD9D9D9),
+                    color: const Color(0xFFD9D9D9),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(15),
                     ),
                   ),
-                ),
-                Container(
-                  width: 14,
-                  height: 89,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 107,
-                        height: 98.54,
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              left: 20.78,
-                              top: 0,
-                              child: Container(
-                                width: 17.11,
-                                height: 89,
-                                decoration: ShapeDecoration(
-                                  color: Color(0xFFFF1515),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(20)),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              left: 0,
-                              top: 0,
-                              child: Container(
-                                width: 17.11,
-                                height: 89,
-                                decoration: ShapeDecoration(
-                                  color: Color(0xFFFF1515),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(20)),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              left: 0,
-                              top: 20.90,
-                              child: Transform(
-                                transform: Matrix4.identity()
-                                  ..translate(0.0, 0.0)
-                                  ..rotateZ(-0.69),
+                  child: SizedBox(
+                    width: 14,
+                    height: 89,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 107,
+                          height: 98.54,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 35.78,
+                                top: 0,
                                 child: Container(
-                                  width: 15.63,
-                                  height: 122.61,
-                                  decoration: ShapeDecoration(
+                                  width: 17.11,
+                                  height: 89,
+                                  decoration: const ShapeDecoration(
                                     color: Color(0xFFFF1515),
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.only(
-                                          bottomLeft: Radius.circular(15)),
+                                          topRight: Radius.circular(20)),
                                     ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
+                              Positioned(
+                                left: 12,
+                                top: 0,
+                                child: Container(
+                                  width: 17.11,
+                                  height: 89,
+                                  decoration: const ShapeDecoration(
+                                    color: Color(0xFFFF1515),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.only(
+                                          topRight: Radius.circular(20)),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: 12,
+                                top: 20.90,
+                                child: Transform(
+                                  transform: Matrix4.identity()
+                                    ..translate(0.0, 0.0)
+                                    ..rotateZ(-0.69),
+                                  child: Container(
+                                    width: 17.11,
+                                    height: 122.61,
+                                    decoration: const ShapeDecoration(
+                                      color: Color(0xFFFF1515),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.only(
+                                            bottomLeft: Radius.circular(20)),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ],
